16149912cc399a1beb3105949b609f45608dc03a
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-04-11 14:56:59 +0200

    troisième pas

--- app/src/main/java/education/cccp/mobile/fragment/MainActivity.kt
+++ app/src/main/java/education/cccp/mobile/fragment/MainActivity.kt
@@ -1,16 +1,20 @@
 package education.cccp.mobile.fragment
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import education.cccp.mobile.fragment.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(),Fragment1.Communication {
-
+class MainActivity : AppCompatActivity(), Fragment1.Communication {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 
     override fun send(name: String) {
-        TODO("Not yet implemented")
+        binding.textViewMainActivity.text = name
     }
+
 }
